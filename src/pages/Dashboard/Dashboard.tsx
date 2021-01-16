0c46f2d55c7698e33226b2506f47b8276fa19f16
dimitrisafendras <dimitris.afendras@gmail.com>
2021-01-16 11:16:57 +0200

    working google auth

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,12 +1,25 @@
 import { PageContent } from '@components/layout';
+import { GoogleLogin } from 'react-google-login';
+
 import { Text } from '@medly-components/core';
 import React from 'react';
 import { Props } from './types';
 
-export const Dashboard: React.SFC<Props> = ({ isLoading }) => (
-    <PageContent isLoading={isLoading}>
-        <Text textWeight="Strong" textVariant="body1">
-            Dashboard Content
-        </Text>
-    </PageContent>
-);
+export const Dashboard: React.SFC<Props> = ({ isLoading }) => {
+    const responseGoogle = (response: object) => {
+        console.log(response);
+    }
+    const clientId = '734655008898-ac7u7rbuq37fs24dt94482bodisfdrlv.apps.googleusercontent.com'
+    return(
+        <PageContent isLoading={isLoading}>
+            <Text textWeight='Strong' textVariant='body1'>
+                <GoogleLogin
+                    clientId={clientId}
+                    buttonText='Login'
+                    onSuccess={responseGoogle}
+                    onFailure={responseGoogle}
+                    cookiePolicy={'single_host_origin'}
+                />
+            </Text>
+        </PageContent>
+)};
